0cc208a5f204621d261bd4c1d009dec2aadce962
victorh3nrique <107193894+victorh3nrique@users.noreply.github.com>
2024-09-17 10:40:39 -0300

    Update common.js

--- graficos/common.js
+++ graficos/common.js
@@ -1,6 +1,4 @@
 const getCSS = (variavel) => {
-    const bodyStyles = getComputedStyle(document.body)
-    return bodyStyles.getPropertyValue(variavel)
     return getComputedStyle(document.body).getPropertyValue(variavel)
 }
 
